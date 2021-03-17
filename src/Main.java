4d0b0f7b58419b497dde420d41249f1ad3a55af5
TSpiridonov <t.a.spiridonob@gmail.com>
2021-03-17 22:20:05 +0300

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,14 @@
-import java.util.*;
-import java.util.regex.*;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        String s1 = "3[xyz]2[xy]6[asd]zxc";
+        String s1 = "3[4[x]xyz]x";
+        String s2 = "3[xasd]4[xasdsd]x";
+        String s3 = "3[]@[xfa]x4yz]x";
+        String s4 = "3[[asxd4[sda]]x";
 
-        List<Integer> repeats = new ArrayList<>();
-
-        Pattern num = Pattern.compile("(\\d+)\\[");
-        Matcher m1 = num.matcher(s1);
-
-        while (m1.find()){
-            repeats.add(Integer.parseInt(m1.group(1)));
-        }
-
-        List<String> rows = new ArrayList<String>();
-        Pattern p = Pattern.compile("\\[([a-z]+)\\]");
-        Matcher m = p.matcher(s1);
-        while (m.find()) {
-            rows.add(m.group(1));
-        }
-
-        List<String> single = new ArrayList<>();
-        Pattern sing = Pattern.compile("\\]([a-z]+)");
-        Matcher m2 = sing.matcher(s1);
-        while (m2.find()){
-            single.add(m2.group(1));
-        }
-
-
-
-        System.out.println(result(repeats,rows,single));
-
-    }
-
-    public static String result(List<Integer> nums, List<String> rows, List<String> single ){
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.get(i); j++) {
-                sb.append(rows.get(i));
-            }
-        }
-
-        if(single.size() > 0){
-            for (String s : single) {
-                sb.append(s);
-            }
-        }
-
-        return sb.toString();
-    }
-
-
-
+        System.out.println(Unwrapper.getResult(s4));
 }
+}
+
